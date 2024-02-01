6087f0da5ea0df0ff806e34f8b363ad045b4fa94
Indemion <147136612+Mastrayane@users.noreply.github.com>
2024-02-01 21:07:50 +0300

    Update search_server.h

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -15,6 +15,7 @@
 
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double EPSILON = 1e-6;
 
 
 class SearchServer {
@@ -120,7 +121,7 @@ std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_quer
 
 	sort(matched_documents.begin(), matched_documents.end(),
 		[](const Document& lhs, const Document& rhs) {
-			if (std::abs(lhs.relevance - rhs.relevance) < 1e-6) {
+			if (std::abs(lhs.relevance - rhs.relevance) < EPSILON) {
 				return lhs.rating > rhs.rating;
 			}
 			else {
